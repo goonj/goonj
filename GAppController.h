53f011d03902e80e998c38266bb17b509fe5da7a
Pratul Kalia <pratul@pratul.in>
2009-10-08 22:58:40 +0530

    Updated contributor copyrights.

--- GAppController.h
+++ GAppController.h
@@ -17,7 +17,8 @@
 	You should have received a copy of the GNU General Public License
 	along with Goonj. If not, see <http://www.gnu.org/licenses/>.
 
-	Copyright 2009 Ankur Sethi.
+    Copyright 2009 Pratul Kalia.
+    Copyright 2009 Ankur Sethi.
 */
 
 #import <Cocoa/Cocoa.h>
--- GMainWindowController.m
+++ GMainWindowController.m
@@ -19,6 +19,7 @@
 	You should have received a copy of the GNU General Public License
 	along with Goonj. If not, see <http://www.gnu.org/licenses/>.
 
+    Copyright 2009 Pratul Kalia.
 	Copyright 2009 Ankur Sethi.
 */
 
