492806cfffe8047d33a790edef479b4246249b63
GeneralMaximus <get.me.ankur@gmail.com>
2009-10-31 00:34:04 +0530

    * Fix header.

--- NSString+GoonjUtils.m
+++ NSString+GoonjUtils.m
@@ -1,10 +1,25 @@
-//
-//  NSString+Utilities.m
-//  Goonj
-//
-//  Created by Ankur Sethi on 23/10/09.
-//  Copyright 2009 The Goonj Project. All rights reserved.
-//
+/*
+	File: NSString+GoonjUtils.h
+	Description: Extends NSString (implementation).
+
+	This file is part of Goonj.
+
+	Goonj is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Goonj is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Goonj. If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009 Pratul Kalia.
+    Copyright 2009 Ankur Sethi.
+*/
 
 #import "NSString+GoonjUtils.h"
 
