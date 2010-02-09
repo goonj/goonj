938023d8579450ed47f5d3325217925ed06711c4
Nandeep Mali <n9986@n-machine-apple.(none)>
2010-02-09 23:15:01 +0530

    Refactored and cleaned lot of code.

--- GPlaybackController.m
+++ GPlaybackController.m
@@ -19,6 +19,7 @@
 
     Copyright 2009 Pratul Kalia.
     Copyright 2009 Ankur Sethi.
+    Copyright 2010 Nandeep Mali.
 */
 
 #import "GPlaybackController.h"
@@ -38,7 +39,6 @@ CAStreamBasicDescription fileFormat;
 Float64 fileDuration;
 UInt64 nPackets;
 
-
 char *nowPlayingFile;
 
 void Play(char *fileURL);
@@ -48,14 +48,9 @@ void Stop(char *fileURL);
 
 bool isPlaying = false;
 
-double PrepareFileAU (CAAudioUnit &au, 
-                      CAStreamBasicDescription &fileFormat, 
-                      AudioFileID audioFile,
-                      UInt64 nPackets,
-                      SInt64 startFrame,
-                      Float64 fileDuration);
+void PrepareFileAU (SInt64 startFrame);
 
-void MakeSimpleGraph (AUGraph &theGraph, CAAudioUnit &fileAU, CAStreamBasicDescription &fileFormat, AudioFileID audioFile);
+void MakeSimpleGraph ();
 
 - (IBAction) playButtonWasClicked:(id)sender
 {
@@ -74,34 +69,45 @@ void MakeSimpleGraph (AUGraph &theGraph, CAAudioUnit &fileAU, CAStreamBasicDescr
 
 void Play(char *fileURL)
 {
-    CFURLRef theURL = CFURLCreateFromFileSystemRepresentation(kCFAllocatorDefault, (UInt8*)fileURL, strlen(fileURL), false);
+    CFURLRef theURL = CFURLCreateFromFileSystemRepresentation(kCFAllocatorDefault, 
+                                                              (UInt8*)fileURL, 
+                                                              strlen(fileURL), 
+                                                              false);
     
-	XThrowIfError (AudioFileOpenURL (theURL, kAudioFileReadPermission, 0, &audioFile), "AudioFileOpenURL");
-    
+	XThrowIfError (AudioFileOpenURL (theURL, 
+                                     kAudioFileReadPermission, 
+                                     0, 
+                                     &audioFile), 
+                   "AudioFileOpenURL");
+        
     // get the number of channels of the file
 	UInt32 propsize = sizeof(CAStreamBasicDescription);
-	XThrowIfError (AudioFileGetProperty(audioFile, kAudioFilePropertyDataFormat, &propsize, &fileFormat), "AudioFileGetProperty");
+	XThrowIfError (AudioFileGetProperty(audioFile, 
+                                        kAudioFilePropertyDataFormat, 
+                                        &propsize, 
+                                        &fileFormat), 
+                   "AudioFileGetProperty");
 	
 	printf ("playing file: %s\n", fileURL);
 	printf ("format: "); fileFormat.Print();
  
     // this makes the graph, the file AU and sets it all up for playing
-	MakeSimpleGraph (theGraph, fileAU, fileFormat, audioFile);
+	MakeSimpleGraph ();
         
     // calculate the duration
 	UInt32 packetSize = sizeof(nPackets);
-	XThrowIfError (AudioFileGetProperty(audioFile, kAudioFilePropertyAudioDataPacketCount, &packetSize, &nPackets), "kAudioFilePropertyAudioDataPacketCount");
+	XThrowIfError (AudioFileGetProperty(audioFile, 
+                                        kAudioFilePropertyAudioDataPacketCount, 
+                                        &packetSize, 
+                                        &nPackets), 
+                   "kAudioFilePropertyAudioDataPacketCount");
     
     fileDuration = (nPackets * fileFormat.mFramesPerPacket) / fileFormat.mSampleRate;
     
     // now we load the file contents up for playback before we start playing
-    // this has to be done the AU is initialized and anytime it is reset or uninitialized
-    fileDuration = PrepareFileAU (fileAU, 
-                                  fileFormat, 
-                                  audioFile,
-                                  nPackets,
-                                  0,
-                                  fileDuration);
+    // this has to be done the AU is initialized and anytime it is reset or 
+    // uninitialized
+    PrepareFileAU (0);
     
 	printf ("file duration: %f secs\n", fileDuration);
     
@@ -124,24 +130,19 @@ void Seek(float seconds)
         
     // now we load the file contents up for playback before we start playing
     // this has to be done the AU is initialized and anytime it is reset or uninitialized
-    fileDuration = PrepareFileAU (fileAU, 
-                                  fileFormat, 
-                                  audioFile,
-                                  nPackets,
-                                  startFrame,
-                                  fileDuration);
+    PrepareFileAU (startFrame);
 }
 
-double PrepareFileAU (CAAudioUnit &au, 
-                      CAStreamBasicDescription &fileFormat, 
-                      AudioFileID audioFile,
-                      UInt64 nPackets,
-                      SInt64 startFrame,
-                      Float64 fileDuration)
+
+
+// This prepares the scheduling of the file playback. Used for starting the
+// playback and seeking as well
+void PrepareFileAU (SInt64 startFrame)
 {	
+    // Reset the Audio Unit to make sure all previous schedules are cleared
+    fileAU.Reset(kAudioUnitScope_Global, 0);
     
-    au.Reset(kAudioUnitScope_Global, 0);
-    
+    // Set up the scheduled region
 	ScheduledAudioFileRegion rgn;
 	memset (&rgn.mTimeStamp, 0, sizeof(rgn.mTimeStamp));
 	rgn.mTimeStamp.mFlags = kAudioTimeStampSampleTimeValid;
@@ -153,71 +154,109 @@ double PrepareFileAU (CAAudioUnit &au,
 	rgn.mStartFrame = startFrame;
 	rgn.mFramesToPlay = UInt32((nPackets - startFrame) * fileFormat.mFramesPerPacket);
     
-    // tell the file player AU to play all of the file
-	XThrowIfError (au.SetProperty (kAudioUnitProperty_ScheduledFileRegion, 
-                                   kAudioUnitScope_Global, 0,&rgn, sizeof(rgn)), "kAudioUnitProperty_ScheduledFileRegion");
+    // tell the file player AU to play the scheduled region
+	XThrowIfError (fileAU.SetProperty (kAudioUnitProperty_ScheduledFileRegion, 
+                                       kAudioUnitScope_Global, 
+                                       0,
+                                       &rgn, 
+                                       sizeof(rgn)), 
+                   "kAudioUnitProperty_ScheduledFileRegion");
 	
     // prime the fp AU with default values
 	UInt32 defaultVal = 0;
-	XThrowIfError (au.SetProperty (kAudioUnitProperty_ScheduledFilePrime, 
-                                   kAudioUnitScope_Global, 0, &defaultVal, sizeof(defaultVal)), "kAudioUnitProperty_ScheduledFilePrime");
+	XThrowIfError (fileAU.SetProperty (kAudioUnitProperty_ScheduledFilePrime, 
+                                       kAudioUnitScope_Global, 
+                                       0, 
+                                       &defaultVal, 
+                                       sizeof(defaultVal)), 
+                   "kAudioUnitProperty_ScheduledFilePrime");
     
-    // tell the fp AU when to start playing (this ts is in the AU's render time stamps; -1 means next render cycle)
+    // tell the fp AU when to start playing (this ts is in the AU's render time 
+    // stamps; -1 means next render cycle)
 	AudioTimeStamp startTime;
 	memset (&startTime, 0, sizeof(startTime));
 	startTime.mFlags = kAudioTimeStampSampleTimeValid;
 	startTime.mSampleTime = -1;
-	XThrowIfError (au.SetProperty(kAudioUnitProperty_ScheduleStartTimeStamp, 
-                                  kAudioUnitScope_Global, 0, &startTime, sizeof(startTime)), "kAudioUnitProperty_ScheduleStartTimeStamp");
-    
-	return fileDuration;
+	XThrowIfError (fileAU.SetProperty(kAudioUnitProperty_ScheduleStartTimeStamp, 
+                                      kAudioUnitScope_Global, 
+                                      0, 
+                                      &startTime, 
+                                      sizeof(startTime)), 
+                   "kAudioUnitProperty_ScheduleStartTimeStamp");
 }
 
 
-
-void MakeSimpleGraph (AUGraph &theGraph, CAAudioUnit &fileAU, CAStreamBasicDescription &fileFormat, AudioFileID audioFile)
+/*
+ Makes a very simple audio graph which connects the output of the file node to 
+ the input of an Output Node that outputs the audio to the default audio device 
+ that the user has selected in the System Preferences. This function also takes 
+ care of audio channel layouts. Probably don't need to bother with that right 
+ now. 
+*/
+void MakeSimpleGraph ()
 {
+    // Create a new graph
 	XThrowIfError (NewAUGraph (&theGraph), "NewAUGraph");
 	
 	CAComponentDescription cd;
     
-	// output node
+	// output node description
 	cd.componentType = kAudioUnitType_Output;
 	cd.componentSubType = kAudioUnitSubType_DefaultOutput;
 	cd.componentManufacturer = kAudioUnitManufacturer_Apple;
     
+    // Add the Output node to the Graph
 	AUNode outputNode;
-	XThrowIfError (AUGraphAddNode (theGraph, &cd, &outputNode), "AUGraphAddNode");
+	XThrowIfError (AUGraphAddNode (theGraph, &cd, &outputNode), 
+                   "AUGraphAddNode");
 	
-	// file AU node
-	AUNode fileNode;
+	// file AU node description
 	cd.componentType = kAudioUnitType_Generator;
 	cd.componentSubType = kAudioUnitSubType_AudioFilePlayer;
 	
-	XThrowIfError (AUGraphAddNode (theGraph, &cd, &fileNode), "AUGraphAddNode");
+    // Add the file node to the graph
+	AUNode fileNode;
+	XThrowIfError (AUGraphAddNode (theGraph, &cd, &fileNode), 
+                   "AUGraphAddNode");
 	
 	// connect & setup
 	XThrowIfError (AUGraphOpen (theGraph), "AUGraphOpen");
 	
 	// install overload listener to detect when something is wrong
 	AudioUnit anAU;
-	XThrowIfError (AUGraphNodeInfo(theGraph, fileNode, NULL, &anAU), "AUGraphNodeInfo");
+	XThrowIfError (AUGraphNodeInfo(theGraph, fileNode, NULL, &anAU), 
+                   "AUGraphNodeInfo");
 	
 	fileAU = CAAudioUnit (fileNode, anAU);
     
     // prepare the file AU for playback
     // set its output channels
-	XThrowIfError (fileAU.SetNumberChannels (kAudioUnitScope_Output, 0, fileFormat.NumberChannels()), "SetNumberChannels");
+	XThrowIfError (fileAU.SetNumberChannels (kAudioUnitScope_Output, 
+                                             0, 
+                                             fileFormat.NumberChannels()), 
+                   "SetNumberChannels");
     
     // set the output sample rate of the file AU to be the same as the file:
-	XThrowIfError (fileAU.SetSampleRate (kAudioUnitScope_Output, 0, fileFormat.mSampleRate), "SetSampleRate");
+	XThrowIfError (fileAU.SetSampleRate (kAudioUnitScope_Output, 
+                                         0, 
+                                         fileFormat.mSampleRate), 
+                   "SetSampleRate");
     
     // load in the file 
 	XThrowIfError (fileAU.SetProperty(kAudioUnitProperty_ScheduledFileIDs, 
-                                      kAudioUnitScope_Global, 0, &audioFile, sizeof(audioFile)), "SetScheduleFile");
-    
-    
-	XThrowIfError (AUGraphConnectNodeInput (theGraph, fileNode, 0, outputNode, 0), "AUGraphConnectNodeInput");
+                                      kAudioUnitScope_Global, 
+                                      0, 
+                                      &audioFile, 
+                                      sizeof(audioFile)), 
+                   "SetScheduleFile");
+        
+    // Connect the fileNode to outputNode
+	XThrowIfError (AUGraphConnectNodeInput (theGraph, 
+                                            fileNode, 
+                                            0, 
+                                            outputNode, 
+                                            0), 
+                   "AUGraphConnectNodeInput");
     
     // AT this point we make sure we have the file player AU initialized
     // this also propogates the output format of the AU to the output unit
@@ -226,24 +265,44 @@ void MakeSimpleGraph (AUGraph &theGraph, CAAudioUnit &fileAU, CAStreamBasicDescr
 	// workaround a race condition in the file player AU
 	usleep (10 * 1000);
     
-    // if we have a surround file, then we should try to tell the output AU what the order of the channels will be
+    // if we have a surround file, then we should try to tell the output AU what
+    // the order of the channels will be
 	if (fileFormat.NumberChannels() > 2) {
 		UInt32 layoutSize = 0;
 		OSStatus err;
-		XThrowIfError (err = AudioFileGetPropertyInfo (audioFile, kAudioFilePropertyChannelLayout, &layoutSize, NULL),
+        
+        // Get the size of the layout
+		XThrowIfError (err = AudioFileGetPropertyInfo (audioFile, 
+                                                       kAudioFilePropertyChannelLayout, 
+                                                       &layoutSize, 
+                                                       NULL),
                        "kAudioFilePropertyChannelLayout");
 		
+        // If everything went well go further
 		if (!err && layoutSize) {
 			char* layout = new char[layoutSize];
-			
-			err = AudioFileGetProperty(audioFile, kAudioFilePropertyChannelLayout, &layoutSize, layout);
+			            
+			err = AudioFileGetProperty(audioFile, 
+                                       kAudioFilePropertyChannelLayout, 
+                                       &layoutSize, 
+                                       layout);
+            
 			XThrowIfError (err, "Get Layout From AudioFile");
 			
 			// ok, now get the output AU and set its layout
-			XThrowIfError (AUGraphNodeInfo(theGraph, outputNode, NULL, &anAU), "AUGraphNodeInfo");
-			
-			err = AudioUnitSetProperty (anAU, kAudioUnitProperty_AudioChannelLayout, 
-                                        kAudioUnitScope_Input, 0, layout, layoutSize);
+			XThrowIfError (AUGraphNodeInfo(theGraph, 
+                                           outputNode, 
+                                           NULL, 
+                                           &anAU), 
+                           "AUGraphNodeInfo");
+        
+            err = AudioUnitSetProperty (anAU, 
+                                        kAudioUnitProperty_AudioChannelLayout, 
+                                        kAudioUnitScope_Input, 
+                                        0, 
+                                        layout, 
+                                        layoutSize);
+            
 			XThrowIfError (err, "kAudioUnitProperty_AudioChannelLayout");
 			
 			delete [] layout;
