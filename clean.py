5a5669867615d7005a8130d68341600e962378aa
GeneralMaximus <get.me.ankur@gmail.com>
2010-02-22 23:30:17 +0530

    You can now do a './clean.py trackdb' to only clean the tracks database.

--- clean.py
+++ clean.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
-import shutil, re, os
+import shutil, os, sys
 
 def Clean():
     goonjDir = os.path.expanduser('~/Library/Application Support/Goonj/')
     goonjPlist = os.path.expanduser('~/Library/Preferences/org.GoonjProject.Goonj.plist')
-    shutil.rmtree(goonjDir)
-    os.remove(goonjPlist)
+    
+    if len(sys.argv) == 1:
+        shutil.rmtree(goonjDir)
+        os.remove(goonjPlist)
+    elif sys.argv[1] == 'trackdb':
+        trackDb = os.path.join(goonjDir, 'tracks.db')
+        os.remove(trackDb)
 
 if __name__ == '__main__':
     Clean()
